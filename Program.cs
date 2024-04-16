eccbb70b9e01fb4249490b3e317a62ac049a845d
DylanCahuMDS <dylan.cahu@my-digital-shcool.org>
2024-04-16 11:02:05 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using PenduConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        gameEngine game = new gameEngine();
+
+
+        while (game.canPlay)
+        {
+
+
+            Console.WriteLine("Guess the word!");
+            Console.WriteLine("You have " + (game.maxWrongGuesses - game.wrongGuesses) + " guesses left.");
+            Console.WriteLine("Current status: " + game.getMaskedWord());
+            Console.WriteLine("Enter your guess: ");
+            char guess = Console.ReadLine().ToLower()[0];
+
+            if (game.checkGuess(guess) == false)
+            {
+                Console.WriteLine("Incorrect!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
+
+        if (game.canPlay == false && game.isLose)
+        {
+            Console.WriteLine("You lose! The word was: " + game.word);
+            Console.ReadLine();
+        }
+        else if (game.canPlay == false && game.isWin)
+        {
+            Console.WriteLine("Congratulations! You win !  The word was: " + game.word);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gameEngine.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PenduConsole
+{
+    public class gameEngine
+    {
+        public int wrongGuesses = 0;
+        public int maxWrongGuesses = 6;
+        public int goodGuessesCount;
+        public string word;
+        public bool[] guessedLetters;
+        public bool isWin = false;
+        public bool isLose = false;
+        public bool canPlay = true;
+
+        // Constructor
+        public gameEngine()
+        {
+            word = getStartWord();
+            guessedLetters = new bool[word.Length];
+        }
+
+        public string getStartWord()
+        {
+            return "test";
+        }
+
+        public bool checkGuess(char guess)
+        {
+            if (word.Contains(guess))
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (word[i] == guess)
+                    {
+                        goodGuessesCount++;
+                        guessedLetters[i] = true;
+                    }
+                }
+
+                if (goodGuessesCount == word.Length)
+                {
+                    canPlay = false;
+                    isWin = true;
+                }
+            }
+            else
+            {
+                wrongGuesses++;
+
+                if (wrongGuesses == maxWrongGuesses)
+                {
+                    canPlay = false;
+                    isLose = true;
+                }
+            }
+            return word.Contains(guess);
+        }
+
+        public string getMaskedWord()
+        {
+            string maskedWord = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == ' ')
+                {
+                    maskedWord += " ";
+                }
+                else
+                {
+                    if (guessedLetters[i])
+                    {
+                        maskedWord += word[i];
+                    }
+                    else
+                    {
+                        maskedWord += "#";
+                    }
+                }
+            }
+            return maskedWord;
+        }
+    }
+   }
